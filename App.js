716974750ff5f19a9b6f26c94c8e091981629dc1
Edward Lin <46078915+EdwardOfLinster@users.noreply.github.com>
2022-03-22 15:26:30 -0700

    Base Project

---
+++ App.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {Helmet} from "react-helmet";
+
+import Home from './pages/Home';
+import Support from './pages/Support';
+import Submission from "./pages/Submission";
+import "./css/reset.css";
+import "./css/main.css";
+
+
+const App = () => {
+  return (
+      <BrowserRouter forceRefresh={true}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="support" element={<Support />} />
+          <Route path="submission" element={<Submission/>} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+
+export default App;
